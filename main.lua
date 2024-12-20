7904c496bf33d790f15a465d0b4f87f7509f4f53
alrice <isaiahlowder06@outlook.com>
2024-12-19 19:52:24 -0600

    testing, update 11 cuz i did 7 twice

--- main.lua
+++ main.lua
@@ -867,7 +867,7 @@ function lib:init(ti, dosplash, visiblekey, deleteprevious)
             toggleswitch.Size = UDim2.new(0, 418, 0, 37)
             toggleswitch.Font = Enum.Font.Gotham
             toggleswitch.Text = name
-            toggleswitch.TextColor3 = Color3.fromRGB(95, 95, 95)
+            toggleswitch.TextColor3 = Color3.fromRGB(125, 125, 125)
             toggleswitch.TextSize = 21
             toggleswitch.TextWrapped = true
             toggleswitch.TextXAlignment = Enum.TextXAlignment.Left
